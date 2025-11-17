ce4f60fc62afddb21bbe893dc6a0960d8695a171
ozlovny <olegsmirnov12220@gmail.com>
2025-11-17 20:53:50 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
-TOKEN = "6894507056:AAEcCdFJRHqzY75Q_sHMWELs5WwIY1KfBWk" # Токен бота
+TOKEN = "8541656886:AAEkLY3oOwgGaO27dQJUlB03v6iaBBilWug" # Токен бота
 PAYMENTS_URL = "https://t.me/+URFxwN-Q_UBiNTky" # Канал выплат
-BETS_ID = -1002238635644 # ID Канала со ставками (НЕ С ПЛАТЕЖАМИ)
-BROKER_ID = -1002238635644 # ID Канала с платежами (НЕ СО СТАВКАМИ)
-VUVOD_ID = -1002149445368 # ID Канала с заявками на вывод
+BETS_ID = -1003465819434 # ID Канала со ставками (НЕ С ПЛАТЕЖАМИ)
+BROKER_ID = -1002406027808 # ID Канала с платежами (НЕ СО СТАВКАМИ)
+VUVOD_ID = -1003426270931 # ID Канала с заявками на вывод
 CHAT_ID = -1002221565734 # ID Чата
 USER_AGREEMENT = "https://telegra.ph/Polzovatelskoe-Soglashenie-proekta-TetherBet-08-28" # Пользовательское соглашение
-STAVKA_URL = "https://t.me/send?start=IV3ZZIjS3gLO" # Ссылка на счёт CryptoBot
+STAVKA_URL = "https://t.me/send?start=IVD7Y5BpR9M2" # Ссылка на счёт CryptoBot
 RULES_URL = "https://t.me/+fMhi5Mkb0mg1MTli" # Ссылка на канал правил
 SUPPORT_URL = "https://t.me/vemorr" # Ссылка на тех поддержку
 NEWS_URL = "https://t.me/+4IErPhfRr2ozN2Y6" # Ссылка на новостной канал
 CHAT_URL = "https://t.me/+YdUvOb4EHGZiMWQy" # Ссылка на чат
-CRYPTOPAY_TOKEN = "239640:AAuCFAyAuZp6NyKv1SqDL28aTPfsl3Y2mEe" # Токен CryptoPay
-ADMINS = [640612893] # ID Администраторов через запятую
+CRYPTOPAY_TOKEN = "358333:AAivRR5Rc0zM0RQDBZ1mcUbUydoinvEF0oz" # Токен CryptoPay
+ADMINS = [6677500867] # ID Администраторов через запятую
 NAME = "Tether Bet" # Название
 TEAM_NAME = "TETHER TEAM" # Название в FAQ
 
